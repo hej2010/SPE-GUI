03f5c60b669cf287d6eea609d702f57dab5fd673
Rikard Teodorsson <rikard128@pm.me>
2021-03-11 17:35:28 +0100

    Parser is working

---
+++ src/main/java/org/example/spe/ParsedFlinkSPE.java
@@ -0,0 +1,24 @@
+package org.example.spe;
+
+import org.example.graph.dag.DirectedGraph;
+import org.jetbrains.annotations.NotNull;
+
+import javax.annotation.Nonnull;
+import java.util.List;
+import java.util.Map;
+
+public class ParsedFlinkSPE extends ParsedSPE {
+
+    ParsedFlinkSPE(@NotNull String name, @NotNull List<ParsedOperator> operators, @NotNull List<String> baseImports, @NotNull List<String> baseDefinition, @NotNull Map<String, List<String>> operatorImportsMap) {
+        super(name, operators, baseImports, baseDefinition, operatorImportsMap);
+    }
+
+    @Nonnull
+    @Override
+    public String generateCodeFrom(@NotNull DirectedGraph directedGraph) {
+
+        return "todo";
+    }
+
+    // TODO the connection method etc is different for each SPE
+}
--- src/main/java/org/example/spe/SPEParser.java
+++ src/main/java/org/example/spe/SPEParser.java
@@ -22,6 +22,13 @@ public class SPEParser {
     private static final String IMPORTS = "imports";
     private static final String DEFINITION = "definition";
     private static final String BASE = "base";
+    private static final String BEFORE = "before";
+    private static final String MIDDLE = "middle";
+    private static final String AFTER = "after";
+    private static final String PLACEHOLDERS = "placeholders";
+    private static final String INPUT = "input";
+    private static final String OUTPUT = "output";
+    private static final String IDENTIFIER = "identifier";
 
     public static void main(String[] args) {
         try {
@@ -39,29 +46,60 @@ public class SPEParser {
         String file = Files.readFile(Paths.get(Paths.get(url.toURI()).toString(), spe.fileName).toString());
 
         JSONObject fileObj = new JSONObject(file);
-        System.out.println(fileObj.toString(2));
+        //System.out.println(fileObj.toString(2));
 
         final String name = fileObj.getString(NAME);
         final List<String> opsList = getStringListFromArr(fileObj.getJSONArray(OPERATORS));
         JSONObject objImports = fileObj.getJSONObject(IMPORTS);
         JSONObject objDefs = fileObj.getJSONObject(DEFINITION);
-        final List<String> baseImports = getValuesIfExists(BASE, objImports);
-        final List<String> baseDefinition = getValuesIfExists(BASE, objDefs);
+        final List<String> baseImports = getValuesIfExistsOrEmpty(BASE, objImports);
+        final List<String> baseDefinition = getValuesIfExistsOrEmpty(BASE, objDefs);
         final Map<String, List<String>> operatorImportsMap = new HashMap<>();
         Map<String, ParsedOperator.Definition> definitionMap = new HashMap<>();
+        List<ParsedOperator> parsedOperators = new LinkedList<>();
         for (String s : opsList) {
             if (objImports.has(s)) {
                 operatorImportsMap.put(s, getStringListFromArr(objImports.getJSONArray(s)));
             }
-            if (objDefs.has(s)) {
-
+            if (!objDefs.has(s)) {
+                System.err.println("Missing definition for " + s + " in " + spe.fileName);
+                //throw new RuntimeException("Missing definition for " + s + " in " + spe.fileName);
+                continue;
             }
+            parsedOperators.add(new ParsedOperator(s, getDefinition(objDefs.getJSONObject(s))));
         }
 
-        return null;
+
+        if (spe == SPE.FLINK) {
+            return new ParsedFlinkSPE(name, parsedOperators, baseImports, baseDefinition, operatorImportsMap);
+        } else if (spe == SPE.LIEBRE) {
+            return new ParsedLiebreSPE(name, parsedOperators, baseImports, baseDefinition, operatorImportsMap);
+        }
+
+        throw new RuntimeException();
     }
 
-    private static List<String> getValuesIfExists(@Nonnull String key, @Nonnull JSONObject o) {
+    @Nonnull
+    private static ParsedOperator.Definition getDefinition(@Nonnull JSONObject o) {
+        String codeBefore = getStringOrEmpty(BEFORE, o);
+        String codeMiddle = getStringOrEmpty(MIDDLE, o);
+        String codeAfter = getStringOrEmpty(AFTER, o);
+        o = o.getJSONObject(PLACEHOLDERS);
+        List<String> input = getStringListFromArr(o.getJSONArray(INPUT));
+        List<String> output = getStringListFromArr(o.getJSONArray(OUTPUT));
+        String identifier = getStringOrEmpty(IDENTIFIER, o);
+        return new ParsedOperator.Definition(codeBefore, codeMiddle, codeAfter, input, output, identifier);
+    }
+
+    private static String getStringOrEmpty(String s, JSONObject o) {
+        if (o.has(s)) {
+            return o.getString(s);
+        }
+        return "";
+    }
+
+    @Nonnull
+    private static List<String> getValuesIfExistsOrEmpty(@Nonnull String key, @Nonnull JSONObject o) {
         List<String> baseDefinition;
         if (o.has(BASE)) {
             baseDefinition = getStringListFromArr(o.getJSONArray(BASE));
