0ebb79ccbd82b28e9b8158e508015643de662bda
Rikard Teodorsson <rikard128@pm.me>
2021-04-08 10:15:29 +0200

    Started on new visualisation fxml

---
+++ src/main/java/gui/controllers/VisController.java
@@ -0,0 +1,15 @@
+package gui.controllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class VisController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+}
